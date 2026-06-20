642776ced22e99f329b9fb48b7ff8a9065a38e1a
Peter Szathmary <peter.szathmary@szathmary.sk>
2026-06-20 17:01:10 +0200

    center wedding date constants in wedding.js, show date on details page

---
+++ src/data/wedding.js
@@ -0,0 +1,8 @@
+export const COUPLE_A  = 'Erika';
+export const COUPLE_B  = 'Peter';
+export const DATE_FULL = '15. mája 2027';
+export const DAY_NAME  = 'Sobota';
+export const LOCATION  = 'KPU, Poltár, Slovensko';
+
+// ISO datetime for the countdown script
+export const DATE_ISO  = '2027-05-15T14:00:00';
